51a03d9cec09f5fa1f03f5a95147caba24d4791e
ALP300 <aitorpalomino1@gmail.com>
2025-01-22 21:41:32 -0500

    Actualizar

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const router= Router();
 router.get('/', (req, res) => res.render('index' ,{title:'Inicio'}));
 router.get('/about', (req, res) => res.render('SobreNosotros' ,{title:'Sobre Nosotros'}));
 router.get('/contact', (req, res) => res.render('Contactanos' ,{title:'Contacto'}));
-
+router.get('/catalogo', (req, res) => res.render('Catalogo' ,{title:'Catálogo'}));
+router.get('/Titanic', (req, res) => res.render('infoTitanic' ,{title:'Titanic'}));
 export default router;
